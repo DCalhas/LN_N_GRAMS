b7a75acc6a71a623fbfcd1d8ba007d7922b13b5f
JGandum <jm.gandumferreira@gmail.com>
2017-11-03 14:44:36 +0000

    criam.final

--- out2final.py
+++ out2final.py
@@ -1,15 +1,15 @@
-out = open("fosseIrSer-2.out")
-final = open("fosseIrSer-2.final", "w")
+out = open("criamCrerCriar.out")
+final = open("criamCrerCriar.final", "w")
 
 lines = out.readlines()
 
 for line in lines:
 	divide = line.split("\t")
 	if "#" not in divide[0] and "?" not in divide[0]:
-		fosse = divide[1].split("fosse")
-		Fosse = divide[1].split("Fosse")
-		if len(fosse) + len(Fosse) < 4:
-			fosseRemoved = divide[1].replace("fosse", divide[0])
-			FosseRemoved = fosseRemoved.replace("Fosse", divide[0])
-			final.write(FosseRemoved)
+		criam = divide[1].split("criam")
+		Criam = divide[1].split("Criam")
+		if len(criam) + len(Criam) < 4:
+			criamRemoved = divide[1].replace("criam", divide[0])
+			CriamRemoved = criamRemoved.replace("Criam", divide[0])
+			final.write(CriamRemoved)
 			
