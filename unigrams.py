fa7cc1bab514089058ffbc69debaf0c3b827c7cf
David Calhas <davidcalhas@hotmail.com>
2017-11-03 14:35:51 +0000

    unigrams

---
+++ unigrams.py
@@ -0,0 +1,25 @@
+import string
+
+final = open("fosseIrSer-2.final")
+
+lines = final.readlines()
+
+wordsOnly = {}
+
+for line in lines:
+	for c in string.punctuation:
+		line = line.replace(c, " ")
+
+	words = line.split(" ")
+
+	for word in words:
+		if word != " " and word != "" and word!= "\n":
+			if not word in wordsOnly:
+				wordsOnly[word] = 1
+			else:
+				wordsOnly[word] = wordsOnly[word] + 1
+
+	
+
+for word in wordsOnly:
+	print(word, wordsOnly[word])
