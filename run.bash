66acae16211433a9aa5615aa63a466f899d09852
David Calhas <davidcalhas@hotmail.com>
2017-11-05 22:35:02 +0000

    everything good

--- run.bash
+++ run.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-python processPhrase.py palavra2Unigramas.txt palavra2Bigramas.txt fosseParametrizacao.txt palavra2Frases.txt > palavra1Resultado.txt
+python processPhrase.py palavra2Unigramas.txt palavra2Bigramas.txt fosseParametrizacao.txt palavra2Frases.txt > palavra2Resultado.txt
+python processPhrase.py palavra1Unigramas.txt palavra1Bigramas.txt criamParametrizacao.txt palavra1Frases.txt > palavra1Resultado.txt
